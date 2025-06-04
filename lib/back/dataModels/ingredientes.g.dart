6f78c1d8f53be13273c21a3ee0bb48488bedc39e
rikich3 <rchambillap@unsa.edu.pe>
2025-06-04 11:20:02 -0500

    realizando los data Models para Hive

---
+++ lib/back/dataModels/ingredientes.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ingredientes.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ingredientesAdapter extends TypeAdapter<ingredientes> {
+  @override
+  final int typeId = 1;
+
+  @override
+  ingredientes read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ingredientes(
+      name: fields[0] as String,
+      descripcion: fields[1] as String,
+      unidadMedida: fields[2] as String,
+      cantidad: fields[3] as int,
+      precio: fields[4] as double,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ingredientes obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.descripcion)
+      ..writeByte(2)
+      ..write(obj.unidadMedida)
+      ..writeByte(3)
+      ..write(obj.cantidad)
+      ..writeByte(4)
+      ..write(obj.precio);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ingredientesAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
