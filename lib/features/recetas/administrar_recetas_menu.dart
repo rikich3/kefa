12598dbb191b80c7485ebb86a4db906cfa8bd4d8
sllachod <sllachod@unsa.edu.pe>
2025-06-17 01:45:21 -0500

    agrega la clase AdministrarRecetasMenu para gestionar la visualización de recetas

---
+++ lib/features/recetas/administrar_recetas_menu.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'administrar_recetas_page.dart';
+
+class AdministrarRecetasMenu extends StatefulWidget {
+  const AdministrarRecetasMenu({Key? key}) : super(key: key);
+
+  @override
+  State<AdministrarRecetasMenu> createState() => _AdministrarRecetasMenuState();
+}
+
+class _AdministrarRecetasMenuState extends State<AdministrarRecetasMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: SizedBox(
+        height: 450,
+        child: AdministrarRecetasPage(),
+      ),
+    );
+  }
+}
